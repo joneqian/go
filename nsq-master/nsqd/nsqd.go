5df9e1a7da11887faf872139a81466d701f4dcc8
joneqian <qian.qing@aliyun.com>
2015-03-20 21:28:11 +0800

    add nsp code

---
+++ nsq-master/nsqd/nsqd.go
@@ -0,0 +1,576 @@
+package nsqd
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net"
+	"os"
+	"path"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/bitly/go-simplejson"
+	"github.com/bitly/nsq/internal/http_api"
+	"github.com/bitly/nsq/internal/lookupd"
+	"github.com/bitly/nsq/internal/protocol"
+	"github.com/bitly/nsq/internal/statsd"
+	"github.com/bitly/nsq/internal/util"
+	"github.com/bitly/nsq/internal/version"
+)
+
+const (
+	TLSNotRequired = iota
+	TLSRequiredExceptHTTP
+	TLSRequired
+)
+
+type NSQD struct {
+	// 64bit atomic vars need to be first for proper alignment on 32bit platforms
+	clientIDSequence int64
+
+	sync.RWMutex
+
+	opts *nsqdOptions
+
+	healthMtx sync.RWMutex
+	healthy   int32
+	err       error
+	startTime time.Time
+
+	topicMap map[string]*Topic
+
+	lookupPeers []*lookupPeer
+
+	tcpAddr       *net.TCPAddr
+	httpAddr      *net.TCPAddr
+	httpsAddr     *net.TCPAddr
+	tcpListener   net.Listener
+	httpListener  net.Listener
+	httpsListener net.Listener
+	tlsConfig     *tls.Config
+
+	idChan     chan MessageID
+	notifyChan chan interface{}
+	exitChan   chan int
+	waitGroup  util.WaitGroupWrapper
+}
+
+func NewNSQD(opts *nsqdOptions) *NSQD {
+	n := &NSQD{
+		opts:       opts,
+		healthy:    1,
+		startTime:  time.Now(),
+		topicMap:   make(map[string]*Topic),
+		idChan:     make(chan MessageID, 4096),
+		exitChan:   make(chan int),
+		notifyChan: make(chan interface{}),
+	}
+
+	if opts.MaxDeflateLevel < 1 || opts.MaxDeflateLevel > 9 {
+		n.logf("FATAL: --max-deflate-level must be [1,9]")
+		os.Exit(1)
+	}
+
+	if opts.ID < 0 || opts.ID >= 4096 {
+		n.logf("FATAL: --worker-id must be [0,4096)")
+		os.Exit(1)
+	}
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", opts.TCPAddress)
+	if err != nil {
+		n.logf("FATAL: failed to resolve TCP address (%s) - %s", opts.TCPAddress, err)
+		os.Exit(1)
+	}
+	n.tcpAddr = tcpAddr
+
+	httpAddr, err := net.ResolveTCPAddr("tcp", opts.HTTPAddress)
+	if err != nil {
+		n.logf("FATAL: failed to resolve HTTP address (%s) - %s", opts.HTTPAddress, err)
+		os.Exit(1)
+	}
+	n.httpAddr = httpAddr
+
+	if opts.HTTPSAddress != "" {
+		httpsAddr, err := net.ResolveTCPAddr("tcp", opts.HTTPSAddress)
+		if err != nil {
+			n.logf("FATAL: failed to resolve HTTPS address (%s) - %s", opts.HTTPSAddress, err)
+			os.Exit(1)
+		}
+		n.httpsAddr = httpsAddr
+	}
+
+	if opts.StatsdPrefix != "" {
+		statsdHostKey := statsd.HostKey(net.JoinHostPort(opts.BroadcastAddress,
+			strconv.Itoa(httpAddr.Port)))
+		prefixWithHost := strings.Replace(opts.StatsdPrefix, "%s", statsdHostKey, -1)
+		if prefixWithHost[len(prefixWithHost)-1] != '.' {
+			prefixWithHost += "."
+		}
+		opts.StatsdPrefix = prefixWithHost
+	}
+
+	if opts.TLSClientAuthPolicy != "" && opts.TLSRequired == TLSNotRequired {
+		opts.TLSRequired = TLSRequired
+	}
+
+	tlsConfig, err := buildTLSConfig(opts)
+	if err != nil {
+		n.logf("FATAL: failed to build TLS config - %s", err)
+		os.Exit(1)
+	}
+	if tlsConfig == nil && n.opts.TLSRequired != TLSNotRequired {
+		n.logf("FATAL: cannot require TLS client connections without TLS key and cert")
+		os.Exit(1)
+	}
+	n.tlsConfig = tlsConfig
+
+	n.waitGroup.Wrap(func() { n.idPump() })
+
+	n.logf(version.String("nsqd"))
+	n.logf("ID: %d", n.opts.ID)
+
+	return n
+}
+
+func (n *NSQD) logf(f string, args ...interface{}) {
+	if n.opts.Logger == nil {
+		return
+	}
+	n.opts.Logger.Output(2, fmt.Sprintf(f, args...))
+}
+
+func (n *NSQD) SetHealth(err error) {
+	n.healthMtx.Lock()
+	defer n.healthMtx.Unlock()
+	n.err = err
+	if err != nil {
+		atomic.StoreInt32(&n.healthy, 0)
+	} else {
+		atomic.StoreInt32(&n.healthy, 1)
+	}
+}
+
+func (n *NSQD) IsHealthy() bool {
+	return atomic.LoadInt32(&n.healthy) == 1
+}
+
+func (n *NSQD) GetError() error {
+	n.healthMtx.RLock()
+	defer n.healthMtx.RUnlock()
+	return n.err
+}
+
+func (n *NSQD) GetHealth() string {
+	if !n.IsHealthy() {
+		return fmt.Sprintf("NOK - %s", n.GetError())
+	}
+	return "OK"
+}
+
+func (n *NSQD) GetStartTime() time.Time {
+	return n.startTime
+}
+
+func (n *NSQD) Main() {
+	var httpListener net.Listener
+	var httpsListener net.Listener
+
+	ctx := &context{n}
+
+	n.waitGroup.Wrap(func() { n.lookupLoop() })
+
+	tcpListener, err := net.Listen("tcp", n.tcpAddr.String())
+	if err != nil {
+		n.logf("FATAL: listen (%s) failed - %s", n.tcpAddr, err)
+		os.Exit(1)
+	}
+	n.tcpListener = tcpListener
+	tcpServer := &tcpServer{ctx: ctx}
+	n.waitGroup.Wrap(func() {
+		protocol.TCPServer(n.tcpListener, tcpServer, n.opts.Logger)
+	})
+
+	if n.tlsConfig != nil && n.httpsAddr != nil {
+		httpsListener, err = tls.Listen("tcp", n.httpsAddr.String(), n.tlsConfig)
+		if err != nil {
+			n.logf("FATAL: listen (%s) failed - %s", n.httpsAddr, err)
+			os.Exit(1)
+		}
+		n.httpsListener = httpsListener
+		httpsServer := &httpServer{
+			ctx:         ctx,
+			tlsEnabled:  true,
+			tlsRequired: true,
+		}
+		n.waitGroup.Wrap(func() {
+			http_api.Serve(n.httpsListener, httpsServer, n.opts.Logger, "HTTPS")
+		})
+	}
+	httpListener, err = net.Listen("tcp", n.httpAddr.String())
+	if err != nil {
+		n.logf("FATAL: listen (%s) failed - %s", n.httpAddr, err)
+		os.Exit(1)
+	}
+	n.httpListener = httpListener
+	httpServer := &httpServer{
+		ctx:         ctx,
+		tlsEnabled:  false,
+		tlsRequired: n.opts.TLSRequired == TLSRequired,
+	}
+	n.waitGroup.Wrap(func() {
+		http_api.Serve(n.httpListener, httpServer, n.opts.Logger, "HTTP")
+	})
+
+	if n.opts.StatsdAddress != "" {
+		n.waitGroup.Wrap(func() { n.statsdLoop() })
+	}
+}
+
+func (n *NSQD) LoadMetadata() {
+	fn := fmt.Sprintf(path.Join(n.opts.DataPath, "nsqd.%d.dat"), n.opts.ID)
+	data, err := ioutil.ReadFile(fn)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			n.logf("ERROR: failed to read channel metadata from %s - %s", fn, err)
+		}
+		return
+	}
+
+	js, err := simplejson.NewJson(data)
+	if err != nil {
+		n.logf("ERROR: failed to parse metadata - %s", err)
+		return
+	}
+
+	topics, err := js.Get("topics").Array()
+	if err != nil {
+		n.logf("ERROR: failed to parse metadata - %s", err)
+		return
+	}
+
+	for ti := range topics {
+		topicJs := js.Get("topics").GetIndex(ti)
+
+		topicName, err := topicJs.Get("name").String()
+		if err != nil {
+			n.logf("ERROR: failed to parse metadata - %s", err)
+			return
+		}
+		if !protocol.IsValidTopicName(topicName) {
+			n.logf("WARNING: skipping creation of invalid topic %s", topicName)
+			continue
+		}
+		topic := n.GetTopic(topicName)
+
+		paused, _ := topicJs.Get("paused").Bool()
+		if paused {
+			topic.Pause()
+		}
+
+		channels, err := topicJs.Get("channels").Array()
+		if err != nil {
+			n.logf("ERROR: failed to parse metadata - %s", err)
+			return
+		}
+
+		for ci := range channels {
+			channelJs := topicJs.Get("channels").GetIndex(ci)
+
+			channelName, err := channelJs.Get("name").String()
+			if err != nil {
+				n.logf("ERROR: failed to parse metadata - %s", err)
+				return
+			}
+			if !protocol.IsValidChannelName(channelName) {
+				n.logf("WARNING: skipping creation of invalid channel %s", channelName)
+				continue
+			}
+			channel := topic.GetChannel(channelName)
+
+			paused, _ = channelJs.Get("paused").Bool()
+			if paused {
+				channel.Pause()
+			}
+		}
+	}
+}
+
+func (n *NSQD) PersistMetadata() error {
+	// persist metadata about what topics/channels we have
+	// so that upon restart we can get back to the same state
+	fileName := fmt.Sprintf(path.Join(n.opts.DataPath, "nsqd.%d.dat"), n.opts.ID)
+	n.logf("NSQ: persisting topic/channel metadata to %s", fileName)
+
+	js := make(map[string]interface{})
+	topics := []interface{}{}
+	for _, topic := range n.topicMap {
+		if topic.ephemeral {
+			continue
+		}
+		topicData := make(map[string]interface{})
+		topicData["name"] = topic.name
+		topicData["paused"] = topic.IsPaused()
+		channels := []interface{}{}
+		topic.Lock()
+		for _, channel := range topic.channelMap {
+			channel.Lock()
+			if channel.ephemeral {
+				channel.Unlock()
+				continue
+			}
+			channelData := make(map[string]interface{})
+			channelData["name"] = channel.name
+			channelData["paused"] = channel.IsPaused()
+			channels = append(channels, channelData)
+			channel.Unlock()
+		}
+		topic.Unlock()
+		topicData["channels"] = channels
+		topics = append(topics, topicData)
+	}
+	js["version"] = version.Binary
+	js["topics"] = topics
+
+	data, err := json.Marshal(&js)
+	if err != nil {
+		return err
+	}
+
+	tmpFileName := fmt.Sprintf("%s.%d.tmp", fileName, rand.Int())
+	f, err := os.OpenFile(tmpFileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		f.Close()
+		return err
+	}
+	f.Sync()
+	f.Close()
+
+	err = atomicRename(tmpFileName, fileName)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (n *NSQD) Exit() {
+	if n.tcpListener != nil {
+		n.tcpListener.Close()
+	}
+
+	if n.httpListener != nil {
+		n.httpListener.Close()
+	}
+
+	if n.httpsListener != nil {
+		n.httpsListener.Close()
+	}
+
+	n.Lock()
+	err := n.PersistMetadata()
+	if err != nil {
+		n.logf("ERROR: failed to persist metadata - %s", err)
+	}
+	n.logf("NSQ: closing topics")
+	for _, topic := range n.topicMap {
+		topic.Close()
+	}
+	n.Unlock()
+
+	// we want to do this last as it closes the idPump (if closed first it
+	// could potentially starve items in process and deadlock)
+	close(n.exitChan)
+	n.waitGroup.Wait()
+}
+
+// GetTopic performs a thread safe operation
+// to return a pointer to a Topic object (potentially new)
+func (n *NSQD) GetTopic(topicName string) *Topic {
+	n.Lock()
+	t, ok := n.topicMap[topicName]
+	if ok {
+		n.Unlock()
+		return t
+	}
+	deleteCallback := func(t *Topic) {
+		n.DeleteExistingTopic(t.name)
+	}
+	t = NewTopic(topicName, &context{n}, deleteCallback)
+	n.topicMap[topicName] = t
+
+	n.logf("TOPIC(%s): created", t.name)
+
+	// release our global nsqd lock, and switch to a more granular topic lock while we init our
+	// channels from lookupd. This blocks concurrent PutMessages to this topic.
+	t.Lock()
+	n.Unlock()
+	// if using lookupd, make a blocking call to get the topics, and immediately create them.
+	// this makes sure that any message received is buffered to the right channels
+	if len(n.lookupPeers) > 0 {
+		channelNames, _ := lookupd.GetLookupdTopicChannels(t.name, n.lookupHTTPAddrs())
+		for _, channelName := range channelNames {
+			if strings.HasSuffix(channelName, "#ephemeral") {
+				// we don't want to pre-create ephemeral channels
+				// because there isn't a client connected
+				continue
+			}
+			t.getOrCreateChannel(channelName)
+		}
+	}
+	t.Unlock()
+
+	// NOTE: I would prefer for this to only happen in topic.GetChannel() but we're special
+	// casing the code above so that we can control the locks such that it is impossible
+	// for a message to be written to a (new) topic while we're looking up channels
+	// from lookupd...
+	//
+	// update messagePump state
+	select {
+	case t.channelUpdateChan <- 1:
+	case <-t.exitChan:
+	}
+	return t
+}
+
+// GetExistingTopic gets a topic only if it exists
+func (n *NSQD) GetExistingTopic(topicName string) (*Topic, error) {
+	n.RLock()
+	defer n.RUnlock()
+	topic, ok := n.topicMap[topicName]
+	if !ok {
+		return nil, errors.New("topic does not exist")
+	}
+	return topic, nil
+}
+
+// DeleteExistingTopic removes a topic only if it exists
+func (n *NSQD) DeleteExistingTopic(topicName string) error {
+	n.RLock()
+	topic, ok := n.topicMap[topicName]
+	if !ok {
+		n.RUnlock()
+		return errors.New("topic does not exist")
+	}
+	n.RUnlock()
+
+	// delete empties all channels and the topic itself before closing
+	// (so that we dont leave any messages around)
+	//
+	// we do this before removing the topic from map below (with no lock)
+	// so that any incoming writes will error and not create a new topic
+	// to enforce ordering
+	topic.Delete()
+
+	n.Lock()
+	delete(n.topicMap, topicName)
+	n.Unlock()
+
+	return nil
+}
+
+func (n *NSQD) idPump() {
+	factory := &guidFactory{}
+	lastError := time.Now()
+	for {
+		id, err := factory.NewGUID(n.opts.ID)
+		if err != nil {
+			now := time.Now()
+			if now.Sub(lastError) > time.Second {
+				// only print the error once/second
+				n.logf("ERROR: %s", err)
+				lastError = now
+			}
+			runtime.Gosched()
+			continue
+		}
+		select {
+		case n.idChan <- id.Hex():
+		case <-n.exitChan:
+			goto exit
+		}
+	}
+
+exit:
+	n.logf("ID: closing")
+}
+
+func (n *NSQD) Notify(v interface{}) {
+	n.waitGroup.Wrap(func() {
+		// by selecting on exitChan we guarantee that
+		// we do not block exit, see issue #123
+		select {
+		case <-n.exitChan:
+		case n.notifyChan <- v:
+			n.Lock()
+			err := n.PersistMetadata()
+			if err != nil {
+				n.logf("ERROR: failed to persist metadata - %s", err)
+			}
+			n.Unlock()
+		}
+	})
+}
+
+func buildTLSConfig(opts *nsqdOptions) (*tls.Config, error) {
+	var tlsConfig *tls.Config
+
+	if opts.TLSCert == "" && opts.TLSKey == "" {
+		return nil, nil
+	}
+
+	tlsClientAuthPolicy := tls.VerifyClientCertIfGiven
+
+	cert, err := tls.LoadX509KeyPair(opts.TLSCert, opts.TLSKey)
+	if err != nil {
+		return nil, err
+	}
+	switch opts.TLSClientAuthPolicy {
+	case "require":
+		tlsClientAuthPolicy = tls.RequireAnyClientCert
+	case "require-verify":
+		tlsClientAuthPolicy = tls.RequireAndVerifyClientCert
+	default:
+		tlsClientAuthPolicy = tls.NoClientCert
+	}
+
+	tlsConfig = &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth:   tlsClientAuthPolicy,
+		MinVersion:   opts.TLSMinVersion,
+		MaxVersion:   tls.VersionTLS12, // enable TLS_FALLBACK_SCSV prior to Go 1.5: https://go-review.googlesource.com/#/c/1776/
+	}
+
+	if opts.TLSRootCAFile != "" {
+		tlsCertPool := x509.NewCertPool()
+		caCertFile, err := ioutil.ReadFile(opts.TLSRootCAFile)
+		if err != nil {
+			return nil, err
+		}
+		if !tlsCertPool.AppendCertsFromPEM(caCertFile) {
+			return nil, errors.New("failed to append certificate to pool")
+		}
+		tlsConfig.ClientCAs = tlsCertPool
+	}
+
+	tlsConfig.BuildNameToCertificate()
+
+	return tlsConfig, nil
+}
+
+func (n *NSQD) IsAuthEnabled() bool {
+	return len(n.opts.AuthHTTPAddresses) != 0
+}
---
+++ nsq-master/nsqlookupd/nsqlookupd.go
@@ -0,0 +1,91 @@
+package nsqlookupd
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/bitly/nsq/internal/http_api"
+	"github.com/bitly/nsq/internal/protocol"
+	"github.com/bitly/nsq/internal/util"
+	"github.com/bitly/nsq/internal/version"
+)
+
+type NSQLookupd struct {
+	opts         *nsqlookupdOptions
+	tcpAddr      *net.TCPAddr
+	httpAddr     *net.TCPAddr
+	tcpListener  net.Listener
+	httpListener net.Listener
+	waitGroup    util.WaitGroupWrapper
+	DB           *RegistrationDB
+}
+
+func NewNSQLookupd(opts *nsqlookupdOptions) *NSQLookupd {
+	n := &NSQLookupd{
+		opts: opts,
+		DB:   NewRegistrationDB(),
+	}
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", opts.TCPAddress)
+	if err != nil {
+		n.logf("FATAL: failed to resolve TCP address (%s) - %s", opts.TCPAddress, err)
+		os.Exit(1)
+	}
+	n.tcpAddr = tcpAddr
+
+	httpAddr, err := net.ResolveTCPAddr("tcp", opts.HTTPAddress)
+	if err != nil {
+		n.logf("FATAL: failed to resolve HTTP address (%s) - %s", opts.HTTPAddress, err)
+		os.Exit(1)
+	}
+	n.httpAddr = httpAddr
+
+	n.logf(version.String("nsqlookupd"))
+
+	return n
+}
+
+func (l *NSQLookupd) logf(f string, args ...interface{}) {
+	if l.opts.Logger == nil {
+		return
+	}
+	l.opts.Logger.Output(2, fmt.Sprintf(f, args...))
+}
+
+func (l *NSQLookupd) Main() {
+	ctx := &Context{l}
+
+	tcpListener, err := net.Listen("tcp", l.tcpAddr.String())
+	if err != nil {
+		l.logf("FATAL: listen (%s) failed - %s", l.tcpAddr, err)
+		os.Exit(1)
+	}
+	l.tcpListener = tcpListener
+	tcpServer := &tcpServer{ctx: ctx}
+	l.waitGroup.Wrap(func() {
+		protocol.TCPServer(tcpListener, tcpServer, l.opts.Logger)
+	})
+
+	httpListener, err := net.Listen("tcp", l.httpAddr.String())
+	if err != nil {
+		l.logf("FATAL: listen (%s) failed - %s", l.httpAddr, err)
+		os.Exit(1)
+	}
+	l.httpListener = httpListener
+	httpServer := &httpServer{ctx: ctx}
+	l.waitGroup.Wrap(func() {
+		http_api.Serve(httpListener, httpServer, l.opts.Logger, "HTTP")
+	})
+}
+
+func (l *NSQLookupd) Exit() {
+	if l.tcpListener != nil {
+		l.tcpListener.Close()
+	}
+
+	if l.httpListener != nil {
+		l.httpListener.Close()
+	}
+	l.waitGroup.Wait()
+}
